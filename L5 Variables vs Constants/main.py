2755133f8c45b70047bcd8ab24176fa1114dfd02
Alex-2504834 <M2504834@middlesbro.ac.uk>
2025-09-29 10:42:30 +0100

    Create main.py

---
+++ L5 Variables vs Constants/main.py
@@ -0,0 +1,58 @@
+def taskOne():
+    DOB = "05/12/2006"
+    patientFirstName = "Alex"
+    patientLastName = "Greenwood"
+    patientHeight = "???"
+
+    print(f"\033[1mPatient Info\033[0m\nPatient Name: {patientFirstName} {patientLastName}\nPatient Height: {patientHeight}\nPatient DOB: {DOB}")
+
+def BMI():
+    userInputHeight = input("Enter Your Height: ")
+    userInputWeight = input("Enter Your Weight: ")
+    try: 
+        ##! math not mathing?
+        bmi = (float(userInputWeight) / (float(userInputHeight) ** 2))
+
+        if bmi >= 40:
+            print(f"Your body mass index is: {bmi} which puts you in the \033[0;31mMorbidly Obese\033[0m group")
+        elif 30 <= bmi >= 39.9:
+            print(f"Your body mass index is: {bmi} which puts you in the \033[0;33mObese\033[0m group")
+        elif 25 <= bmi >= 29.9:
+            print(f"Your body mass index is: {bmi} which puts you in the \033[1;33mOver Weight\033[0m group")
+        elif 18.5 <= bmi >= 24.9:
+            print(f"Your body mass index is: {bmi} which puts you in the \033[0;32mNormal Weight\033[0m group")
+        elif bmi <= 18.5:
+            print(f"Your body mass index is: {bmi} which puts you in the \033[0;36mUnder Weight\033[0m group")
+
+        else:
+            print(f"Your body mass index is: {bmi} which puts you in the \033[0;31mUnknown\033[0m group. Your BMI might be too high or low")
+
+                    
+    except ValueError or TypeError:
+        print(f"\033[31mEnter A Numbers\033[0m")
+
+def dosage():
+    import os
+    MAXDOSAGE = 5
+    currentDosage = 0
+    alive = True
+    while alive:
+        os.system("cls")
+        print(f"Patients current dosage is: {currentDosage}")
+        userInput = input("Give the patient a dose? (y/n): ")
+
+        if userInput.lower() == ("y"):
+            currentDosage += 1
+            print(f"You gave them a dose, their current amount is: {currentDosage}" )
+        elif userInput.lower() == ("n"):
+            break
+        else: 
+            pass
+        if currentDosage == 5:
+            print(f"\033[0;31mPatient has reached their max dose.\033[0m")
+        if currentDosage > 5:
+            print(f"\033[0;31mPatient is over the max dose......\033[0m")
+            alive = False
+
+def medicalBilling():
+    pass
