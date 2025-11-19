960f040f41a3de8017dd95bcc28c36b6c89a8ae0
Alex-2504834 <M2504834@middlesbro.ac.uk>
2025-11-19 16:36:26 +0000

    Update main.py

--- L18/main.py
+++ L18/main.py
@@ -17,20 +17,16 @@ import random
 import time
 
 currentClass = {
-    0: {"name": "conner", "weight": 1},
-    1: {"name": "alex", "weight": 2},
-    2: {"name": "kye", "weight": 3},
-    3: {"name": "thomas", "weight": 4},
-    4: {"name": "kieran", "weight": 5}
+    0: {"name": "conner", "weight": 0.5},
+    1: {"name": "alex", "weight": 0.5},
+    2: {"name": "kye", "weight": 0.5},
+    3: {"name": "thomas", "weight": 0.5},
+    4: {"name": "kieran", "weight": 0.5}
 }  
 
-#for index in currentClass:
-#    print(currentClass[index]["name"])
 nameList = [currentClass[index]["name"] for index in currentClass]
 weightList = [currentClass[index]["weight"] for index in currentClass]
 
-#print(nameList)
-#print(weightList)
 
 def weightFromName(name):        
     return [currentClass[each]["weight"] for each in currentClass if currentClass[each]["name"] == name][0]
@@ -39,16 +35,27 @@ def keyFromName(name):
     return [each for each in currentClass if currentClass[each]["name"] == name][0]
 
 def reduceWeight(key, reduceAmount):
-    if (currentClass[key]["weight"] - reduceAmount) == 0:
+    if not (currentClass[key]["weight"] - reduceAmount) < 0:
         currentClass[key].update({"weight": currentClass[key]["weight"] - reduceAmount})
 
 def getRandomStudent():
     randomStudent = random.choices(nameList, weights=weightList)[0]
     print(randomStudent)
     print(weightFromName(randomStudent))
-    reduceWeight(keyFromName(randomStudent), 1)
+    reduceWeight(keyFromName(randomStudent), 0.1)
+    return randomStudent
 #getRandomStudent()
-
+temp = {   
+    "conner": 0,
+    "alex": 0,
+    "kye": 0,
+    "thomas": 0,
+    "kieran": 0
+}
 while True:
-    getRandomStudent()
-    time.sleep(0.25)
+
+    RandomStudent = getRandomStudent()
+    if not (currentClass[keyFromName(RandomStudent)]["weight"]) <= 0:
+        temp.update({f"{RandomStudent}": temp[RandomStudent] + 1})
+    print(temp)
+    time.sleep(0.01)
