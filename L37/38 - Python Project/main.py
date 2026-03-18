23641c927f6c970cd7bc94264461b89b13321187
Alex <M2504834@middlesbro.ac.uk>
2026-03-18 16:50:34 +0000

    Update main.py

--- L37/38 - Python Project/main.py
+++ L37/38 - Python Project/main.py
@@ -1,5 +1,7 @@
 import os
 import time
+import random
+
 mazeSizeX, mazeSizeY = 10, 10
 
 mazeMap = [
@@ -20,7 +22,6 @@ mazeMap = [
     ]
 
 
-player = (1, 1)
 
 def clear() -> None:
 	os.system("cls" if os.name == "nt" else "clear")
@@ -32,19 +33,52 @@ def displayMaze(maze):
                 print("")
             print(cells, end="")
     print("\n")
-def setPlayerPos(player, map):
+
+def setPlayerPos(player, map, lastPos):
+    map[lastPos[1]][lastPos[0]] = "."
     map[player[1]][player[0]] = "P"
     return map
 
+def temp(player, x, y):
+    #?[1] = X [0] = Y
+    return (player[0] + x, player[1] + y)
+
+
 def main() -> None:
-    
+    player = (1,1)
+    lastPos = player
     running=True
     while running:
-        
-        maze = mazeMap
-    
-        maze = setPlayerPos(player=player, map=maze)
+        maze = setPlayerPos(player=player, map=mazeMap, lastPos=lastPos)
         displayMaze(maze)
+        playerInput = input("Enter direction (N, E, S, W): ").upper().strip()
+        match playerInput:
+            case "N":
+                if mazeMap[player[1]-1][player[0]] == "#":
+                    continue
+                lastPos = player
+                player = temp(player, 0, -1)
+
+            case "E":
+                if mazeMap[player[1]][player[0]+1] == "#":
+                    continue
+                lastPos = player
+                player = temp(player, 1, 0)
+
+            case "S":
+                if mazeMap[player[1]+1][player[0]] == "#":
+                    continue
+                lastPos = player
+                player = temp(player, 0, 1)
+
+            case "W":
+                if mazeMap[player[1]][player[0]-1] == "#":
+                    continue
+                lastPos = player
+                player = temp(player, -1, 0)
+            case _:
+                pass
+
         time.sleep(0.2)
         clear()
 
