3188ce4055f9247d9b896bf2bf99727e3543a457
Alex-2504834 <M2504834@middlesbro.ac.uk>
2026-03-25 15:42:32 +0000

    .

--- L38 - Checksum/main.py
+++ L38 - Checksum/main.py
@@ -1,16 +1,21 @@
 def main():
     userInput = input("Enter parcel code: ")
     if len(userInput) == 7:   
+        try:
+            lastDigit = userInput[-1:]
+            restOfDigit = userInput[:-1]
+            checkSum = 0
+            for index, value in enumerate(restOfDigit):
+                checkSum = checkSum + ((index + 1) * int(value))
 
-        lastDigit = userInput[-1:]
-        restOfDigit = userInput[:-1]
-        print(f"{restOfDigit = }")
-        print(f"{lastDigit = }")
+            if (checkSum % 10) == int(lastDigit):
+                print("Is Valid")
+            else:
+                print("Is Not Valid")
+        except ValueError:
+            print("Please Enter A Number!")
 
-        checkSum = 0
-        for index, value in enumerate(restOfDigit):
-            checkSum = checkSum + ((index + 1) * int(value))
-        print(checkSum)
+
 if __name__ == "__main__":
     try:
         main()
